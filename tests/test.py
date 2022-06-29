033ec9cc5c92915619b0e01b5ccee1ffb3b453de
anastasofpv <37800897+anastasofpv@users.noreply.github.com>
2022-06-29 12:09:28 +0300

    Create test.py

---
+++ tests/test.py
@@ -0,0 +1,48 @@
+import datetime
+import time
+import random
+import pymongo
+import requests
+
+
+
+
+
+url = "http://localhost:3000/api/map"
+for i in range(10):
+    droneID = random.randrange(0,3)
+    temperature = random.randrange(16,30)
+    if (droneID == 0):
+        point = [random.randrange(37,40),random.randrange(19,22)]
+    elif (droneID == 1):
+        point = [random.randrange(27,30),random.randrange(19,22)]
+    elif (droneID == 2):
+        point = [random.randrange(17,20),random.randrange(19,22)]
+
+    mydict = {
+        "droneID": droneID,
+        "temperature": temperature,
+        # "timeCreated": datetime.datetime.now(),
+        "point": point
+    }
+
+    x = requests.post(url,json=mydict)
+    # print(mydict)
+    # print("document inserted")
+    time.sleep(3)
+
+
+
+time.sleep(5)
+
+
+myclient = pymongo.MongoClient("localhost", 27017, username="admin", password="drone-db")
+mydb = myclient["droneDB"]
+mycol = mydb["droneData"]
+
+counter = 0
+for x in mycol.find():
+    # print(x)
+    counter += 1
+
+print(counter)
